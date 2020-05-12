d3795efa8cdfbe1af48c70365a05182deaf83074
Xiaofeng Wang <xfun68@gmail.com>
2020-05-12 20:59:41 +0800

    Initial commit

---
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.capability.gtb.demospringconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
