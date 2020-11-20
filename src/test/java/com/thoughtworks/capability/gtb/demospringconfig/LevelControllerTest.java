c42aada06d7f13a4b90129739e78ac29ce394bc1
fufangyuan <zyfcffy@163.com>
2020-11-20 10:58:05 +0800

    [fufangyuan]feat:finish p2s3

---
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTest.java
@@ -0,0 +1,26 @@
+package com.thoughtworks.capability.gtb.demospringconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class LevelControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void should_return_advanced_when_get_level_with_level_num_0() throws Exception {
+        mockMvc.perform(get("/level")).andExpect(jsonPath("$", is("basic")));
+    }
+
+}
